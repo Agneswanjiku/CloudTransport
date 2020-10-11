48480b1ab1e6828650386bf522c8b123e7f20fda
edithntinyari <emothegi@gmail.com>
2020-10-11 17:04:21 +0300

    added boookingseatsactivity.xml

--- app/src/main/java/com/moringaschool/mycloudtransports/BookingSeats.java
+++ app/src/main/java/com/moringaschool/mycloudtransports/BookingSeats.java
@@ -1,4 +1,13 @@
 package com.moringaschool.mycloudtransports;
 
-public class BookingSeats {
-}
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookingSeats extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookingseatsactivity);
+
+    }
