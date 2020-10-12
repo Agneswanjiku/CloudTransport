0c7f259c0fd09bcf76aff67bc456b937e0c9ad0e
edithntinyari <emothegi@gmail.com>
2020-10-12 04:08:02 +0300

    added changes

--- app/src/main/java/com/moringaschool/mycloudtransports/BookingSeatsActivity.java
+++ app/src/main/java/com/moringaschool/mycloudtransports/BookingSeatsActivity.java
@@ -1,20 +1,40 @@
-package com.moringaschool.mycloudtransports;
-
-import android.os.Bundle;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class BookingSeatsActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bookingseatsactivity);
-
-
-        radioGroup = findViewById(R.id.radioGroup);
-        textView = findViewById(R.id.text_view_selected);
-        Button buttonApply = findViewById(R.id.button_apply);
-
-    }
-}
+//package com.moringaschool.mycloudtransports;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class BookingSeatsActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.bookingseatsactivity);
+//
+//
+//        radioGroup = findViewById(R.id.radioGroup);
+//        textView = findViewById(R.id.text_view_selected);
+//        Button buttonApply = findViewById(R.id.button_apply);
+//
+//        @Override
+//        public void onClick (View v){
+//            int radioId = radioGroup.getCheckedRadioButtonId();
+//            radioButton = findViewById(radioId);
+//
+//            textView.setText("Your choice" + radioButton.getText());
+//        }
+//    });
+//}
+//
+//    public  void checkedButton(View view){
+//        int radioId = radioGroup.getCheckedRadioButtonId();
+//        radioButton = findViewById(radioId);
+//
+//        Toast.makeText(this, "Selected Radio Button" + radioButton.getText(), Toast.LENGTH_SHORT).show();
+//    }
+//}
+//
+//    }
+//}
--- app/src/main/java/com/moringaschool/mycloudtransports/SelectionSeatsActivity.java
+++ app/src/main/java/com/moringaschool/mycloudtransports/SelectionSeatsActivity.java
@@ -1,45 +1,45 @@
-package com.moringaschool.mycloudtransports;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SelectionSeatsActivity extends AppCompatActivity {
-
-    RadioGroup radioGroup;
-    RadioButton radioButton;
-    TextView textView;
-    private Object v;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.selectionseatsactivity);
-
-        radioGroup = findViewById(R.id.radioGroup);
-        textView = findViewById(R.id.text_view_selected);
-        Button buttonApply = findViewById(R.id.button_apply);
-
-        @Override
-        public void onClick (View v){
-            int radioId = radioGroup.getCheckedRadioButtonId();
-            radioButton = findViewById(radioId);
-
-            textView.setText("Your choice" + radioButton.getText());
-        }
-    });
-}
-
-public  void checkedButton(View view){
-    int radioId = radioGroup.getCheckedRadioButtonId();
-    radioButton = findViewById(radioId);
-
-    Toast.makeText(this, "Selected Radio Button" + radioButton.getText(), Toast.LENGTH_SHORT).show();
-}
-}
+//package com.moringaschool.mycloudtransports;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.RadioButton;
+//import android.widget.RadioGroup;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class SelectionSeatsActivity extends AppCompatActivity {
+//
+//    RadioGroup radioGroup;
+//    RadioButton radioButton;
+//    TextView textView;
+//    private Object v;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.selectionseatsactivity);
+//
+//        radioGroup = findViewById(R.id.radioGroup);
+//        textView = findViewById(R.id.text_view_selected);
+//        Button buttonApply = findViewById(R.id.button_apply);
+//
+//        @Override
+//        public void onClick (View v){
+//            int radioId = radioGroup.getCheckedRadioButtonId();
+//            radioButton = findViewById(radioId);
+//
+//            textView.setText("Your choice" + radioButton.getText());
+//        }
+//    });
+//}
+//
+//public  void checkedButton(View view){
+//    int radioId = radioGroup.getCheckedRadioButtonId();
+//    radioButton = findViewById(radioId);
+//
+//    Toast.makeText(this, "Selected Radio Button" + radioButton.getText(), Toast.LENGTH_SHORT).show();
+//}
+//}
