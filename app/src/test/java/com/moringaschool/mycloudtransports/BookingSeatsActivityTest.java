7bdcc284152bf27ea5720f1953b837b14a54123e
edithntinyari <emothegi@gmail.com>
2020-10-13 12:11:32 +0300

    added test in bookingseatsActivity

--- app/src/test/java/com/moringaschool/mycloudtransports/BookingSeatsActivityTest.java
+++ app/src/test/java/com/moringaschool/mycloudtransports/BookingSeatsActivityTest.java
@@ -1,5 +1,6 @@
 package com.moringaschool.mycloudtransports;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.preference.TwoStatePreference;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.RadioGroup;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
 
 import static org.junit.Assert.*;
 import static org.robolectric.Shadows.shadowOf;
@@ -29,6 +32,18 @@ public class BookingSeatsActivityTest {
 //        radioButton.setButtonDrawable(R.drawable.busmage);
 //        assertThat(shadowOf(radioButton).finalize()).isEqualTo(R.drawable.clearbus);
     }
+
+    @Test
+    public void clickingSubmit_shouldStartSubmitActivity() {
+        BookingSeatsActivity activity = Robolectric.setupActivity(BookingSeatsActivity.class);
+        activity.findViewById(R.id.buttonSubmit).performClick();
+
+        Intent expectedIntent = new Intent(activity, SelectionVehicleActivity.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+    }
+
+
 
     @Test
     public void getButtonDrawable() {
